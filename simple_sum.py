b396e0b9f0e033555a600ad95b314a6a2c8b16ff
Tony <tonywu9401@gmail.com>
2019-03-28 19:24:24 -0700

    Fixed the bug

--- simple_sum.py
+++ simple_sum.py
@@ -2,7 +2,7 @@
 # Returns the sum of a and b
 def simple_sum(a: int, b: int) -> int:
     # Something seems... wrong...
-    return a + a
+    return a + b
     
 
 print("2 + 3 = {0}".format(simple_sum(2,3)))
